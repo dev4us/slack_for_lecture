ae24a2b7dc087e1199e8587699fd0dac0818ed4e
dev4us <bronxofsoul@gmail.com>
2019-12-08 14:38:31 +0900

    [BE] set PubSub & create your first Subscription

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 
 import connection from "./ormConfig";
 import schema from "./schema";
 
-const server = new GraphQLServer({ schema });
+const pubSub = new PubSub();
+const server = new GraphQLServer({ schema, context: { pubSub } });
 
 connection.then(() =>
   server.start(() => console.log("Server is running on localhost:4000"))
