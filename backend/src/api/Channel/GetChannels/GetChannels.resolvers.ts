7b13789ee693855dcf3448e614cdbfe8de1f13a9
dev4us <bronxofsoul@gmail.com>
2019-12-08 14:47:35 +0900

    [BE] Create & Get Channels

---
+++ backend/src/api/Channel/GetChannels/GetChannels.resolvers.ts
@@ -0,0 +1,35 @@
+import { Resolvers } from "../../../../src/types/resolvers";
+import { GetChannelsResponse } from "src/types/graphql";
+import Channel from "../../../../src/entities/Channel";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetChannels: async (_, __): Promise<GetChannelsResponse> => {
+      try {
+        const channels = await Channel.find();
+
+        if (channels) {
+          return {
+            ok: true,
+            error: null,
+            channels
+          };
+        } else {
+          return {
+            ok: false,
+            error: "채널이 존재하지 않습니다.",
+            channels: null
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          channels: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
--- backend/src/types/graphql.d.ts
+++ backend/src/types/graphql.d.ts
@@ -1,7 +1,8 @@
-export const typeDefs = ["type Channel {\n  id: Int!\n  channelName: String!\n  messages: [Message]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype GetMessagesResponse {\n  ok: Boolean!\n  error: String\n  messages: [Message]\n}\n\ntype Query {\n  GetMessages(innerChannelId: Int!): GetMessagesResponse!\n}\n\ntype SendMessageResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype Mutation {\n  SendMessage(nickname: String!, contents: String!, innerChannelId: Int!): SendMessageResponse!\n}\n\ntype Message {\n  id: Int!\n  nickname: String!\n  contents: String!\n  innerChannel: Channel!\n  innerChannelId: Int!\n  createdAt: String!\n  updatedAt: String\n}\n"];
+export const typeDefs = ["type CreateChannelResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype Mutation {\n  CreateChannel(channelName: String!): CreateChannelResponse!\n  SendMessage(nickname: String!, contents: String!, innerChannelId: Int!): SendMessageResponse!\n}\n\ntype GetChannelsResponse {\n  ok: Boolean!\n  error: String\n  channels: [Channel]\n}\n\ntype Query {\n  GetChannels: GetChannelsResponse!\n  GetMessages(innerChannelId: Int!): GetMessagesResponse!\n}\n\ntype Channel {\n  id: Int!\n  channelName: String!\n  messages: [Message]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype GetMessagesResponse {\n  ok: Boolean!\n  error: String\n  messages: [Message]\n}\n\ntype SendMessageResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype Subscription {\n  SendMessageSubscription: Message\n}\n\ntype Message {\n  id: Int!\n  nickname: String!\n  contents: String!\n  innerChannel: Channel!\n  innerChannelId: Int!\n  createdAt: String!\n  updatedAt: String\n}\n"];
 /* tslint:disable */
 
 export interface Query {
+  GetChannels: GetChannelsResponse;
   GetMessages: GetMessagesResponse;
 }
 
@@ -9,10 +10,18 @@ export interface GetMessagesQueryArgs {
   innerChannelId: number;
 }
 
-export interface GetMessagesResponse {
+export interface GetChannelsResponse {
   ok: boolean;
   error: string | null;
+  channels: Array<Channel> | null;
+}
+
+export interface Channel {
+  id: number;
+  channelName: string;
   messages: Array<Message> | null;
+  createdAt: string;
+  updatedAt: string | null;
 }
 
 export interface Message {
@@ -25,16 +34,19 @@ export interface Message {
   updatedAt: string | null;
 }
 
-export interface Channel {
-  id: number;
-  channelName: string;
+export interface GetMessagesResponse {
+  ok: boolean;
+  error: string | null;
   messages: Array<Message> | null;
-  createdAt: string;
-  updatedAt: string | null;
 }
 
 export interface Mutation {
+  CreateChannel: CreateChannelResponse;
   SendMessage: SendMessageResponse;
+}
+
+export interface CreateChannelMutationArgs {
+  channelName: string;
 }
 
 export interface SendMessageMutationArgs {
@@ -43,7 +55,16 @@ export interface SendMessageMutationArgs {
   innerChannelId: number;
 }
 
+export interface CreateChannelResponse {
+  ok: boolean;
+  error: string | null;
+}
+
 export interface SendMessageResponse {
   ok: boolean;
   error: string | null;
+}
+
+export interface Subscription {
+  SendMessageSubscription: Message | null;
 }
