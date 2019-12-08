786487cf161cd788043dceac124f4ceb3832bb08
dev4us <bronxofsoul@gmail.com>
2019-12-08 15:42:13 +0900

    [FE] UI - ChannelList & App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+import ChannelList from "./Components/ChannelList";
 
 function App() {
-  return <div className="App">hey</div>;
+  return (
+    <MainFrame>
+      <ChannelList />
+    </MainFrame>
+  );
 }
+
+const MainFrame = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: row;
+`;
 
 export default App;
