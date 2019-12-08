0a656aa110bc64364a40408933b568ab7611469a
dev4us <bronxofsoul@gmail.com>
2019-12-08 13:45:40 +0900

    [BE] Connect Database

---
+++ backend/src/ormConfig.ts
@@ -0,0 +1,21 @@
+import { createConnection, Connection } from "typeorm";
+import { ConnectionOptions } from "typeorm";
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectionOptions: ConnectionOptions = {
+  type: "postgres",
+  database: "slack_for_lecture",
+  synchronize: true,
+  logging: true,
+  entities: ["entities/**/*.*"],
+  host: process.env.DB_ENDPOINT,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+};
+
+const connection: Promise<Connection> = createConnection(connectionOptions);
+
+export default connection;
